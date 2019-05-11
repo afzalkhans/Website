cfc5f7c92428d039b96cd2bf97bf378add338168
afzalkhans <36788072+afzalkhans@users.noreply.github.com>
2019-05-11 22:51:11 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,70 @@
+<?php include ("includes/config.php");?>
+
+ <!DOCTYPE html>
+
+<html lang="en">
+<head>
+	<title>   spring 2019  </title>
+<?php
+
+    include ("includes/meta-css.php");
+?>
+
+</head>
+<body> 
+	<?php
+   
+   include ("includes/header.php");
+   include("includes/nav.php");
+
+
+	?>
+
+    <div class="cover-img cover-img-home"></div>
+ 	<section class="layout-width layout-padding">
+
+
+ 		<div class="same">
+        		<h2>Our Products </h2>
+           
+       <p>Enabling the change for Software Product Engineering
+The methods established since long for software product development have changed significantly. Led by the change in the definition of a software product itself, Capgemini enables the positive impact for
+The technology landscape around the product: modernization, architecture, seamless, cross-channel experience for users
+The process of software product development: adoption of agile, DevOps, Design Thinking, continuous integration
+The tools usage across the product development lifecycle: integrated tools, traceability from requirements to versions
+Efficiency Across the PDLC
+Reduced time to market and R&D cost optimization are key to the success for ISVs. Capgemini’s experience and proven approach focusing on the products mindset provide compelling value proposition to software product owners for best leverage of costs and efforts across the product lifecycle.
+Our RightShore model provides the required scalability for new and emerging technologies while providing the flexibility to adapt to the change in customer preferences.
+Aligned Business Models</p>
+        	</div>
+        	<div class="same">
+        	<h2>Our Online Projects</h2>
+            <p>
+       One of the most important ways to develop your skills and improve your employability as a software engineer is to work on side projects. These can run the gamut from open source projects to helpful tools and scripts, to fun toys.
+       While some of our colleagues always seem to be working on a new side project, it’s not so easy for the rest of us. We want to work on a side project but struggle to come up with a good idea.
+        In my experience, this happens when we subconsciously artificially restrict ourselves. For example, we may unintentionally limit the types of projects that we feel are ‘worthy’ as side projects.
+        By setting the bar too high — like aiming to create a successful, popular, and original open source library — we rob ourselves of the opportunity to have fun and learn something from a less ambitious, but still worthwhile, side project.
+         In this post, I want to help fight “side project paralysis” by offering up 40 side project ideas that any software developer can start in one weekend. The emphasis here is not on projects that are going to change the world. Instead, these are fun projects to give you something to add to your portfolio and make you a better developer</p>
+        	</div>
+        	<div class="same">
+                <h2>Our Online Work</h2>
+            
+      <p>Most of the members of the Young Entrepreneurs Council swear by this platform when they want to assemble a team that’s both brilliant and qualified.
+
+        A lot of people recommend UpWork.com which is good if you need a one-time gig, but if you plan on being a long-term part time “freelancer” and since you’re a software engineer, there won’t really be a shortage of work for you to find.
+
+        My advice with UpWork is to continuously send your proposals, and don’t think that the first proposal will be the only one that you’ll need to send; on average, it takes at least 7 days for you to land a contract.
+
+        If you’re really good at what you do it probably won’t matter, but my advice is to scatter your resumé all over the inter webs, and make profiles on other sites like Freelancer as well…
+
+        I’ve actually written about how potential clients prepare to look for freelancers, and what you as a freelancers can highlight to set yourself apart from the competition; you can check it (it’s free)</p>
+
+
+        	</div>
+           
+	</section>
+<?php
+include("includes/footer.php");
+?>
+</body>
+</html>
