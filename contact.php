cb245279cc401e0e83bc83438e9e5624d261853a
afzalkhans <36788072+afzalkhans@users.noreply.github.com>
2019-05-12 23:04:21 +0500

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,53 @@
+<?php include ("includes/config.php");?>
+ <!DOCTYPE html>
+
+<html lang="en">
+<head>
+	<title>   spring 2019 > contact </title>
+<?php
+
+    include ("includes/meta-css.php");
+?>
+
+</head>
+<body> 
+	<?php
+   
+   include ("includes/header.php");
+   //include("includes/nav.php");
+
+
+	?>
+
+    <!-- <div class="cover-img cover-img-home"></div> -->
+ 		
+
+<div class="contact_main">
+			<h1> FEEDBACK FORM</h1>
+		<form  class="main-forms" >
+			<input type="text" name="fn" placeholder="Full Name*" required /><br>
+			<input type="text" name="adrs" placeholder="Address*" required /><br>
+			<input type="text" name="mob" placeholder="Contact*" required /><br>
+			<input type="email" name="em" placeholder="Email Address*" required /><br>
+				<textarea name="msg" placeholder="feedback us"></textarea><br>
+				<input type="submit" value="Send Message">
+						</form>
+
+
+     <div id="adrees">
+     	<h2> Software house Islamabad </h2>
+<p><strong><b>Address</b>:</strong>  Street 53- i-10 - Sohni  Road, Islamabad , Pakistan.</p>
+<p><strong><b>Phone</b>:</strong>     +92 323 8913896</p>
+<p><b><strong>Mobile/Whatsapp</strong>:    </b> +92 322 8080150</p>
+<p><b>Email:       </b>afzalfaheem262@gmail.com</p>
+<p><b>Skype:</b>onlinetechie</p>
+     	
+     </div>
+</div>
+		
+	</section>
+<?php
+include("includes/footer.php");
+?>
+</body>
+</html>
